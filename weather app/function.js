e990a8668f7d8ce54417185f7aede52c1b67244f
Joemn <Aruljoe37@gmail.com>
2021-02-01 06:27:44 +0530

    weather app half done

--- weather app/function.js
+++ weather app/function.js
@@ -1,13 +1,33 @@
-const getGeoLoc = (stringLoc) => {
-  let mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${stringLoc}.json?access_token=pk.eyJ1Ijoiam9lbW9uMDB4IiwiYSI6ImNra2l5dWw0djFyeHUyeHF0Y2ZsZzF0aW4ifQ.bZnKp4_zuSXkiR0YYzwlXA&limit=1`;
+const request = require("postman-request");
 
-  request({ url: mapboxUrl, json: true }, (err, res, body) => {
-    let lattitude = body.features[0].geometry.coordinates[1];
-    let longitude = body.features[0].geometry.coordinates[0];
-    console.log(
-      `Your coordinates latitude  ${lattitude} & langitude ${longitude}`
-    );
+const getGeoCode = (address, callback) => {
+  let url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+    address
+  )}.json?access_token=pk.eyJ1Ijoiam9lbW9uMDB4IiwiYSI6ImNra2l5dWw0djFyeHUyeHF0Y2ZsZzF0aW4ifQ.bZnKp4_zuSXkiR0YYzwlXA&limit=1`;
+  request({ url, json: true }, (err, res, body) => {
+    if (err) {
+      callback("Unable to connect to network", undefined);
+    } else if (body.features.length == 0) {
+      callback("No such place ", undefined);
+    } else {
+      callback(undefined, {
+        lattitude: body.features[0].geometry.coordinates[1],
+        longitude: body.features[0].geometry.coordinates[0],
+        placeName: body.features[0].place_name,
+      });
+    }
   });
 };
 
-module.exports = { getGeoLoc };
+const getWeatherData = (lat, long, callback) => {
+  let url = `http://api.weatherstack.com/current?access_key=67c165f0096dfa421735a106e07c751f&query=${lat},${long}
+  `;
+  request({ url, json: true }, (err, res, body) => {
+    if (err) {
+      callback("Unknown error", undefined);
+    } else {
+      callback(undefined, body);
+    }
+  });
+};
+module.exports = { getGeoCode, getWeatherData };
--- weather app/index.js
+++ weather app/index.js
@@ -1,42 +1,13 @@
-let request = require("postman-request");
+let { getGeoCode } = require("./function");
+let { getWeatherData } = require("./function");
 
-let mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/chennai.json?access_token=pk.eyJ1Ijoiam9lbW9uMDB4IiwiYSI6ImNra2l5dWw0djFyeHUyeHF0Y2ZsZzF0aW4ifQ.bZnKp4_zuSXkiR0YYzwlXA&limit=1`;
+// uninistall yargs
 
-request({ url: mapboxUrl, json: true }, (err, res, body) => {
-  let lattitude = body.features[0].geometry.coordinates[1];
-  let longitude = body.features[0].geometry.coordinates[0];
-  console.log(
-    `Your coordinates latitude  ${lattitude} & langitude ${longitude}`
-  );
+getGeoCode("colachel", (err, data) => {
+  console.log("====================");
+  //dataFrom mapbox to get longitude and lattitude
+  // console.log(data);
+  getWeatherData(data.lattitude, data.longitude, (err, data) => {
+    console.log(data);
+  });
 });
-
-// let url =
-//   "http://api.weatherstack.com/current?access_key=67c165f0096dfa421735a106e07c751f&query=london";
-
-// request({ url, json: true }, (err, res, body) => {
-//   console.log(
-//     `temp is ${body.current.temperature} and wind speed is ${body.current.wind_speed}`
-//   );
-//   console.log(body.current.temperature);
-// });
-
-// Yargs
-
-// let yargs = require("yargs");
-// const { command, demandOption } = require("yargs");
-// yargs.command({
-//   command: "weather",
-//   describe: "weather",
-//   builder: {
-//     location: {
-//       describe: "location to search",
-//       demandOption: true,
-//       type: "string",
-//     },
-//   },
-//   handler({ location }) {
-//     log()
-//   },
-// });
-
-// yargs.parse();
