524faa3377ca6cd30d905a77e3cf27790961dd2b
Joemn <Aruljoe37@gmail.com>
2021-02-02 05:55:31 +0530

    async using postman-req is done

--- weather app/index.js
+++ weather app/index.js
@@ -3,11 +3,20 @@ let { getWeatherData } = require("./function");
 
 // uninistall yargs
 
-getGeoCode("colachel", (err, data) => {
-  console.log("====================");
-  //dataFrom mapbox to get longitude and lattitude
-  // console.log(data);
+let location = process.argv[2];
+
+if (!location) {
+  return console.log("Enter proper place name");
+}
+
+getGeoCode(location, (err, data) => {
+  if (err) {
+    return console.log("error");
+  }
   getWeatherData(data.lattitude, data.longitude, (err, data) => {
+    if (err) {
+      return console.log("error");
+    }
     console.log(data);
   });
 });
