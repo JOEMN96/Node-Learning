a115d41b652aa0481b5621e54ccbbb1b72cb16ca
Joemn <Aruljoe37@gmail.com>
2021-02-04 06:48:47 +0530

    web server with express --> learned about handlebars

--- web server/src/app.js
+++ web server/src/app.js
@@ -1,14 +1,34 @@
 const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
 const publicFolder = path.join(__dirname, "../public/");
+const viewsPath = path.join(__dirname, "../templates/views/");
+const partialsPath = path.join(__dirname, "../templates/components");
 
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 app.use(express.static(publicFolder));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Home",
+  });
+});
+
 app.get("/weather", (req, res) => {
-  res.send({ dat: "hello", temp: 100 });
+  res.render("weather", { weather: "hello", title: "weather" });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", { test: "help joe", title: "help" });
+});
+
+app.get("*", (req, res) => {
+  res.render("404");
 });
 
 app.listen(3000, () => {
