309f4a5e1524da28969b74e5d285e1301798f698
Joemn <Aruljoe37@gmail.com>
2021-02-10 06:49:10 +0530

    U for Update in CRUD

--- Task app/mongoTest.js
+++ Task app/mongoTest.js
@@ -10,7 +10,20 @@ MongoClient.connect(url,{useNewUrlParser:true, useUnifiedTopology: true },(err,c
         const db = client.db(dbName)
         console.log("Db connected sucessfully");
       
-        db.collection('tasks').find({completed:true}).toArray((err,data)=>{
-            console.log(data);
-        })
+        // db.collection('tasks').find({completed:true}).toArray((err,data)=>{
+        //     console.log(data);
+        // })
+        // db.collection('users').updateOne({_id:new ObjectID("6021d98824c0562830eede9a")},{
+        //   $inc:{
+        //     age:-1
+        //   }
+        // }).then(data => console.log("Updated")).catch(err => console.log(err))
+
+        db.collection('tasks').updateMany({completed:true},{
+          $set:{
+            completed:false
+          }
+        }).then(resolved => console.log('done'))
+          .catch(err => console.log(err))
+
 })
