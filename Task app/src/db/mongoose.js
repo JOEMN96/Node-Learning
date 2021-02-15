a95752a622803f2427207107c3cc9f85799f4d60
Joemn <Aruljoe37@gmail.com>
2021-02-15 06:40:58 +0530

    async pattern && PATCH (UPDATE)

--- Task app/src/db/mongoose.js
+++ Task app/src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/task-app', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost/task-app', {useNewUrlParser: true, useUnifiedTopology: true,useFindAndModify:false});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
--- Task app/src/index.js
+++ Task app/src/index.js
@@ -14,63 +14,111 @@ app.use(express.json())
 
 // User Creation
 
-app.post('/user',(req,res)=>{
+app.post('/user',async (req,res)=>{
+
   const user = new User(req.body)
-  user.save()
-  .then(()=>  res.status(201).send(user))
-  .catch(err =>  {
-    res.staus(400)
-    res.sendt(err)}
-    )
+
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (err) {
+    res.status(400)
+    res.send(err)
+  }
+})
+
+// GET users 
+
+app.get('/users',async(req,res)=>{
+
+  try {
+    let data =  await User.find({})
+    res.send(data)  
+  } catch (error) {
+    res.status(500).send(error)
+  }
+
+})
+
+// GET Unique user
+app.get('/users/:id',async (req,res)=>{
+    let _id = req.params.id;  
+
+    try {
+      const user = await  User.findById(_id);
+      if(!user) {
+        return res.status(404).send()
+      } 
+      res.send(user)
+    } catch (error) {
+      res.status(404).send("No user Found")
+    }
+})
+
+// UPDATE A Unique User ---> PATCH (UPDATE In CRUD)
+
+app.patch('/users/:id',async (req,res) => {
+
+  let needsUpadate = Object.keys(req.body);
+  let validUpdates = ['name',"password","email","age"]
+
+  let canUpdate = needsUpadate.every(update => validUpdates.includes(update))
+
+  if(!canUpdate) {
+    return res.status(404).send('unable to find Key')
+  }
+  
+    try {
+        let updatedUser = await User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators:true})
+        if(!updatedUser) {
+          return res.status(404).send()
+        }
+        res.send(updatedUser)
+    } catch (e) {
+      res.status(400).send("server Error")
+    }
 })
 
 // Task post Route
 
-app.post('/task',(req,res)=>{
-    const task = new Task(req.body)
-    task.save()
-      .then( () => res.status(201).send(task))
-      .catch((err) => {
-        res.status(400)
-        res.send(err)
-      })
+app.post('/task',async (req,res)=>{
+  const task = new Task(req.body)
+
+    try {
+      await task.save()
+      res.status(201).send(task)
+    } catch (error) {
+      res.status(400)
+      res.send(error)
+    }
 }) 
 
-// GET users 
-
-app.get('/users',(req,res)=>{
-  User.find({}).then(data => res.send(data))
-    .catch(err => res.status(500).send(err)) 
-})
-
-// GET Unique user
-app.get('/users/:id',(req,res)=>{
-    let _id = req.params.id;  
-    User.findById(_id).then((user) => {
-      if(!user) {
-        console.log("no user");
-        return res.status(404).send()
-      } 
-      res.send(user)
-    }).catch(() => {
-      res.status(404).send()
-    })
-})
-
 // GET All Tasks
-app.get('/tasks',(req,res) =>{
-  Task.find({})
-    .then(data => res.send(data))
-    .catch(err => res.status(500).send(err))
+app.get('/tasks',async(req,res) =>{
+  
+    try {
+      const data = await Task.find({})
+      res.send(data)
+    } catch (err) {
+      res.status(500).send(err)
+    }
+
 })
 
 // GET Specific Task
 
-app.get('/task/:id',(req,res) => {
-  let id = req.params.id;
-  Task.findById(id)
-    .then(data => res.send(data))
-    .catch((_) => res.status(403).send("Unable to Find The Task"))
+app.get('/task/:id',async (req,res) => {
+  
+  let id =  req.params.id;
+  try {
+    let data =  await Task.findById(id)
+    if(!data) {
+      return res.status(404).send("No recods Found !")
+    }
+    res.send(data)
+  } catch (err) {
+    res.status(403).send("Unable to Find The Task")
+  }
 })
 
 
